a7b031fe2eacbabec8458be5f03cfa8bd78d6205
Abolarin Olanrewaju Olabode <waju.abolarin@gmail.com>
2019-01-10 07:26:28 +0100

    fixing env variables

--- plugins/api.js
+++ plugins/api.js
@@ -1,8 +1,7 @@
-import 'dotenv/config'
 import Sanity from '~/helpers/api'
 
 console.log(process.env)
 export default (ctx, inject) => {
-  inject('api', () => new Sanity(ctx.env.SANITY_PROJECT_ID, ctx.env.SANITY_DATA_SET ))
+  inject('api', () => new Sanity('11y83f9z', 'production'))
 }
 
