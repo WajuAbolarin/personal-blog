ddfa63783c2e4614be040d2d2a22122a4cd26b73
Abolarin Olanrewaju <waju.abolari@gmail.com>
2019-08-06 22:54:23 +0100

    use env variables

--- helpers/stringHelpers.js
+++ helpers/stringHelpers.js
@@ -1,7 +1,6 @@
 export const upperFirst = val => {
-  let valArray = val.split('')
-  valArray[0] = valArray[0].toUpperCase()
-  val = valArray.join('')
-  return val
-}
-
+  let valArray = val.split("");
+  valArray[0] = valArray[0].toUpperCase();
+  val = valArray.join("");
+  return val;
+};
